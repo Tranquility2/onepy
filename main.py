660c83e16b5417ad5b8a2d3b132f6dfff51db80d
Roy Moore <roy@moore.co.il>
2021-02-06 02:07:30 +0200

    update code for windows

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import readline # optional, will allow Up/Down/History in the console
+import os
+if os.name == 'nt':
+    from pyreadline import Readline
+    readline = Readline()
+else:
+    import readline # optional, will allow Up/Down/History in the console
 import code
+
 variables = globals().copy()
 variables.update(locals())
 shell = code.InteractiveConsole(variables)
 shell.interact()
-
